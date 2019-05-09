965ab24ffc7fdfe8f12f321cb796086d6d657343
Niels <nielsdikken@gmail.com>
2019-05-09 23:42:32 +0200

    Niels Date/Time working:)

--- js/script.js
+++ js/script.js
@@ -1,43 +1,35 @@
-// window.onload = function () {
-//     // Get the modal
-//     var modal = document.getElementById('myModal');
-
-//     // Get the button that opens the modal
-//     var btn = document.getElementById("myBtn");
-
-//     // Get the <span> element that closes the modal
-//     var span = document.getElementsByClassName("close")[0];
-
-//     // When the user clicks the button, open the modal 
-//     btn.onclick = function () {
-//         modal.style.display = "block";
-//     }
-
-//     // When the user clicks on <span> (x), close the modal
-//     span.onclick = function () {
-//         modal.style.display = "none";
-//     }
-
-//     // When the user clicks anywhere outside of the modal, close it
-//     window.onclick = function (event) {
-//         if (event.target == modal) {
-//             modal.style.display = "none";
-//         }
-//     }
-// }
-
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
     // executes when HTML-Document is loaded and DOM is ready
-   console.log("document is ready");
-     
-     
-     jQuery('.btn[href^=#]').click(function(e){
-       e.preventDefault();
-       var href = jQuery(this).attr('href');
-       jQuery(href).modal('toggle');
-     });
-   
-     
-   
-   });  
-     
+    console.log("document is ready");
+    
+    $('#basicExample').timepicker({'timeFormat': 'H:i','step': 15,'scrollDefault': 'now'});
+
+    jQuery('.btn[href^=#]').click(function (e) {
+        e.preventDefault();
+        var href = jQuery(this).attr('href');
+        jQuery(href).modal('toggle');
+    });
+});
+
+window.onload = function () {
+    var picker = new Pikaday({
+        field: document.getElementById('datepicker'),
+        format: 'D/M/YYYY',
+        toString(date, format) {
+            // you should do formatting based on the passed format,
+            // but we will just return 'D/M/YYYY' for simplicity
+            const day = date.getDate();
+            const month = date.getMonth() + 1;
+            const year = date.getFullYear();
+            return `${day}/${month}/${year}`;
+        },
+        parse(dateString, format) {
+            // dateString is the result of `toString` method
+            const parts = dateString.split('/');
+            const day = parseInt(parts[0], 10);
+            const month = parseInt(parts[1], 10) - 1;
+            const year = parseInt(parts[2], 10);
+            return new Date(year, month, day);
+        }
+    });
+}
