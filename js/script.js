18f5aa9b94fcb07f52cc212d4449d7339df57fad
Niels <nielsdikken@gmail.com>
2019-05-13 16:44:16 +0200

    Changes Niels

--- js/script.js
+++ js/script.js
@@ -8,20 +8,48 @@ jQuery(document).ready(function () {
     $('#selectTime').on('changeTime', function () {
         $('#onselectTarget').text($(this).val());
     });
-    
+
     $('.durationExample').timepicker({
-        'timeFormat': 'H:i', 
+        'timeFormat': 'H:i',
         'step': 15,
         'minTime': '14:00',
         'maxTime': '00:00',
         'showDuration': true
     });
 
-    jQuery('.btn[href^=#]').click(function (e) {
-        e.preventDefault();
-        var href = jQuery(this).attr('href');
-        jQuery(href).modal('toggle');
+    $(function () {
+
+        // Get the form fields and hidden div
+        var checkbox = $("#trigger");
+        var hidden = $("#hidden_fields");
+
+        // Hide the fields.
+        // Use JS to do this in case the user doesn't have JS 
+        // enabled.
+        hidden.hide();
+
+        // Setup an event listener for when the state of the 
+        // checkbox changes.
+        checkbox.change(function () {
+            // Check to see if the checkbox is checked.
+            // If it is, show the fields and populate the input.
+            // If not, hide the fields.
+            if (checkbox.is(':checked')) {
+                // Show the hidden fields.
+                hidden.show();
+            } else {
+                // Make sure that the hidden fields are indeed
+                // hidden.
+                hidden.hide();
+            }
+        });
     });
+
+    // jQuery('.btn[href^=#]').click(function (e) {
+    //     e.preventDefault();
+    //     var href = jQuery(this).attr('href');
+    //     jQuery(href).modal('toggle');
+    // });
 });
 
 window.onload = function () {
@@ -45,4 +73,14 @@ window.onload = function () {
             return new Date(year, month, day);
         }
     });
+}
+
+function myFunction() {
+    var checkBox = document.getElementById("myCheck");
+    var text = document.getElementById("text");
+    if (checkBox.checked == true) {
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
 }
