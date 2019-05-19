8c6cf32bf3b8e1dad07f39568e9037b88fa51a8b
Niels <nielsdikken@gmail.com>
2019-05-19 12:59:22 +0200

    Changes Niels, different design and create event task

--- js/script.js
+++ js/script.js
@@ -85,3 +85,11 @@ function myFunction() {
         text.style.display = "none";
     }
 }
+
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
